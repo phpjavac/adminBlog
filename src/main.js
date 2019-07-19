72914eeabc281be2cb5d5fc86b173bbea0f14372
yiziluoying <1060854705@qq.com>
2019-07-19 23:44:56 +0800

    index.vue

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 import api from './axios/install'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './utils/route'
 Vue.use(api);
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -25,7 +25,7 @@ export default {
           }
         })
         .catch(error=>{
-          console.log(error)
+          this.$message.error(error.errorText);
         })
     }
   },
